d1cfc4d3818a72a1c393c152f2d0933525ed1d4d
Benjamin Torfu <btorfu@proton.me>
2025-05-25 11:38:15 +0000

    working on protected route

--- todo/views.py
+++ todo/views.py
@@ -4,11 +4,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import RegisterSerializers, LoginSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
 
 
 class RegisterView(APIView):
     def post(self, request):
-        serializer = RegisterSerializer(data = request.data)
+        serializer = RegisterSerializers(data = request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
@@ -17,3 +18,10 @@ class RegisterView(APIView):
 
 class LoginView(TokenObtainPairView):
     serializer_class = LoginSerializer
+
+
+class MyView(APIView):
+    permission_class = [IsAuthenticated]
+
+    def get(self, request):
+        pass
